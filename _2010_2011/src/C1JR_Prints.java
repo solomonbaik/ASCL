86b0b09480586237723465e37314db3ded5a6f10
baiks <baiks@192.168.1.44>
2019-07-23 16:24:36 -0700

    2010 - 2011 Prints

--- _2010_2011/src/C1JR_Prints.java
+++ _2010_2011/src/C1JR_Prints.java
@@ -1,15 +1,14 @@
 import java.util.*;
 import java.io.*;
-public class C1JR_Prints {
-	public static int evens, odds;
-	
+public class C1JR_Prints {	
 	public static void main(String[] args) {
 		
 		try {
 			
-			Scanner in = new Scanner(new File("0809c1jr.in"));
+			Scanner in = new Scanner(new File("1011c1jr.in"));
 			
 			for(int x = 0; x < 7; x++) {
+				int evens = 0, odds = 0, numerator = 0, denominator = 0;
 				
 				String[] temp = in.nextLine().split(" ");
 				int[] ans = new int[temp.length];
@@ -18,13 +17,40 @@ public class C1JR_Prints {
 					ans[i] = Integer.parseInt(temp[i]);
 				}
 				
+				if(ans[0] == 0 && ans.length == 1) {
+					System.out.println("1/1");
+					continue;
+				}
+				
 				for(int i = 0; i < ans.length; i++) {
 					if(ans[i] % 2 == 0) {
 						evens += 1;
+						if(ans[i] == 10)
+							numerator += 1;
+						if(ans[i] == 8)
+							numerator += 2;
+						if(ans[i] == 6)
+							numerator += 4;
+						if(ans[i] == 4)
+							numerator += 8;
+						if(ans[i] == 2)
+							numerator+= 16;
 					}else {
 						odds += 1;
+						if(ans[i] == 9)
+							denominator += 1;
+						if(ans[i] == 7)
+							denominator += 2;
+						if(ans[i] == 5)
+							denominator += 4;
+						if(ans[i] == 3)
+							denominator += 8;
+						if(ans[i] == 1)
+							denominator += 16;
 					}
 				}
+				
+				System.out.println(numerator + 1 + "/" + (denominator + 1));
 				
 			}
 			
@@ -34,9 +60,4 @@ public class C1JR_Prints {
 		}
 		
 	}
-	
-	public static void whorlNumber() {
-		
-	}
-
 }
