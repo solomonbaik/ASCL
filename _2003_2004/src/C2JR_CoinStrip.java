3304dc6d3d3c60d67e5f1db6d2e1590471068f14
baiks <baiks@192.168.1.44>
2019-08-08 14:17:34 -0700

    coinstrip

---
+++ _2003_2004/src/C2JR_CoinStrip.java
@@ -0,0 +1,102 @@
+import java.util.*;
+import java.io.*;
+public class C2JR_CoinStrip {
+	
+	public static void main(String[] args) {
+		try {
+				Scanner in = new Scanner(new File("0304c2jr.in"));
+				for(int x = 0; x < 9; x++) {
+					int squares = in.nextInt();
+					int coinNum = in.nextInt();
+					int _1 = 0;
+					int _2 = 0;
+					int _3 = 0;
+					int _4 = 0;
+					int _5 = 0;
+					int before = in.nextInt();
+					int current = in.nextInt();
+		
+					if(before - 0 == 2) {
+						_1++;
+					} else if(before - 0 == 3) {
+						_2++;
+						_1++;
+					} else if(before - 0 == 4) {
+						_3++;
+						_2++;
+						_1++;
+					} else if(before - 0 == 5) {
+						_4++;
+						_3++;
+						_2++;
+						_1++;
+					} else if(before - 0 == 6) {
+						_5++;
+						_4++;
+						_3++;
+						_2++;
+						_1++;
+					}
+					if(current - before == 2) {
+						_1++;
+					} 
+					else if(current - before == 3) {
+						_2++;
+						_1++;
+					} 
+					else if(current - before == 4) {
+						_3++;
+						_2++;
+						_1++;
+					}
+					else if(current - before == 5) {
+						_4++;
+						_3++;
+						_2++;
+						_1++;
+					} 
+					else if(current - before == 6) {
+						_5++;
+						_4++;
+						_3++;
+						_2++;
+						_1++;
+					}
+					for(int i = 2; i < coinNum; i++) {
+						before = current;
+						current = in.nextInt();
+						if(current - before == 2) {
+							_1++;
+						}
+						else if(current - before == 3) {
+							_2++;
+							_1++;
+						} 
+						else if(current - before == 4) {
+							_3++;
+							_2++;
+							_1++;
+						} 
+						else if(current - before == 5) {
+							_4++;
+							_3++;
+							_2++;
+							_1++;
+						} 
+						else if(current - before == 6) {
+							_5++;
+							_4++;
+							_3++;
+							_2++;
+							_1++;
+						}
+					}
+					System.out.print(_1 + " " + _2 + " " + _3 + " " + _4 + " " + _5);
+					System.out.println();
+				}
+			}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
